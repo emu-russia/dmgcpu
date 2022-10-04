58f320cabbb17346791b10b2f92ba90e0790db6c
ogamespec <ogamespec@gmail.com>
2022-10-04 23:01:17 +0300

    Script for generating Verilog based on NAND tree description.

---
+++ Scripts/MakeNandTree.py
@@ -0,0 +1,33 @@
+"""
+
+	Script for generating Verilog based on NAND tree description.
+
+	Example tree: "{alu0}{w24,nIR3,nIR4,nIR5}{w10,IR5}{w10,IR4}{w10,IR3}"
+
+"""
+
+import os
+import sys
+import argparse
+import re
+
+def Main(args):
+	terms = re.split (r'[{}]', args.tree);
+	first = True;
+	text = "";
+	for t in terms:
+		if t != "":
+			numbers = re.compile(r'(\d+)')
+			t = numbers.sub(r'[\1]', t)
+			if not first:
+				text += " | ";
+			text += "(" + t.replace(",", "&") + ")";
+			first = False;
+	text = "\tassign " + args.output_name + " = ~(" + text + ");";
+	print (text);
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Specify the tree in the format {terms}{terms}{...}')
+	parser.add_argument('--tree', dest='tree', default='{}', help='NAND tree in Wiki format.')
+	parser.add_argument('--output_name', dest='output_name', default='x', help='Verilog output scalar name')
+	Main(parser.parse_args())
