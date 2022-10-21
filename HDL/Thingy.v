c6facdfbc48e099fca5739fa82ffd59cf2cc1f59
ogamespec <ogamespec@gmail.com>
2022-10-21 12:08:43 +0300

    Thingy BTT -> TempZ

--- HDL/Thingy.v
+++ HDL/Thingy.v
@@ -1,10 +1,10 @@
 
-module Thingy ( w, ALU_to_Thingy, WR, BTT, TTB1, TTB2, TTB3, Thingy_to_bot, bot_to_Thingy );
+module Thingy ( w, ALU_to_Thingy, WR, Temp_Z, TTB1, TTB2, TTB3, Thingy_to_bot, bot_to_Thingy );
 
 	input [40:0] w;
 	input ALU_to_Thingy;
 	input WR;
-	input BTT;
+	input Temp_Z;		// Flag Z from temp Z register (zbus[7])
 	output TTB1;
 	output TTB2;
 	output TTB3;
@@ -14,8 +14,8 @@ module Thingy ( w, ALU_to_Thingy, WR, BTT, TTB1, TTB2, TTB3, Thingy_to_bot, bot_
 	wire t1;
 	wire t2;
 
-	assign t1 = ~(w[8] & ALU_to_Thingy & ~BTT);
-	assign t2 = ~(w[8] & ~ALU_to_Thingy & BTT);
+	assign t1 = ~(w[8] & ALU_to_Thingy & ~Temp_Z);
+	assign t2 = ~(w[8] & ~ALU_to_Thingy & Temp_Z);
 	assign TTB1 = ~(t1 & t2);
 	assign TTB2 = ~(~w[35] & t2);
 	assign TTB3 = ~(~w[31] & t1);
