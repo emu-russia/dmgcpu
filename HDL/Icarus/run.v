468b086104b83da242fc05a5409582ae2f24942f
ogamespec <ogamespec@gmail.com>
2022-10-24 22:28:22 +0300

    Disabled reset in test run

--- HDL/Icarus/run.v
+++ HDL/Icarus/run.v
@@ -100,9 +100,9 @@ module SM83_Run();
 		$dumpfile("dmg_waves.vcd");
 		$dumpvars(0, dmgcore);
 
-		ExternalRESET <= 1'b1;
-		repeat (8) @ (posedge CLK);
-		ExternalRESET <= 1'b0;
+		//ExternalRESET <= 1'b1;
+		//repeat (8) @ (posedge CLK);
+		//ExternalRESET <= 1'b0;
 
 		repeat (64) @ (posedge CLK);
 		$finish;
