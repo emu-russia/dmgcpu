e341f8e4495ef8cc3436643a21e2a0b93ad318ae
org <ogamespec@gmail.com>
2024-03-23 09:31:22 +0300

    test_alu

---
+++ HDL/Icarus/UnitTest/test_alu.bat
@@ -0,0 +1,2 @@
+iverilog -D ICARUS -o test_alu.run ../../_GekkioNames.v ../../ALU.v test_alu.v
+vvp test_alu.run
---
+++ HDL/Icarus/UnitTest/test_alu.v
@@ -0,0 +1,10 @@
+// Basic functional test of the SM83's CLA  (#229)
+
+// Perform cyclic addition of a and b. (values run 0..255)
+
+`timescale 1ns/1ns
+
+module test_alu ();
+
+
+endmodule // test_alu
