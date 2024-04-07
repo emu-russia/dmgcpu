d9cc590782c5ddd35b719d24a66488d8a77f390b
org <ogamespec@gmail.com>
2024-04-07 12:07:34 +0300

    cpu_instrs

--- HDL/Icarus/roms/Readme.md
+++ HDL/Icarus/roms/Readme.md
@@ -3,3 +3,12 @@
 This folder contains small programs for testing the execution of instructions.
 
 HW for the test tool contains just flat 64 KByte memory, these images are loaded at address 0 (see `Bogus_HW` module)
+
+## cpu_insts
+
+Taken from here: https://github.com/retrio/gb-test-roms/tree/master/cpu_instrs
+
+Converted by:
+```
+python3 bin2mem.py cpu_instr.gb cpu_instr.mem
+```
---
+++ Scripts/bin2mem.py
@@ -0,0 +1,26 @@
+'''
+	A script to convert a binary file into Verilog mem format.
+
+'''
+
+import os
+import sys
+
+def Main (file_bin, file_mem):
+	f = open(file_bin, 'rb')
+	binarycontent = f.read(-1)
+	f.close()
+
+	text = ""
+	for b in binarycontent:
+		text += ("%0x" % b) + " "
+
+	text_file = open(file_mem, "w")
+	text_file.write(text)
+	text_file.close()
+
+if __name__ == '__main__':
+	if (len(sys.argv) < 2):
+		print ("Use: python3 bin2mem.py <file.bin> <file.mem>")
+	else:
+		Main(sys.argv[1], sys.argv[2])
