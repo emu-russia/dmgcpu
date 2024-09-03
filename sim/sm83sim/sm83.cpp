59bdf6947530da1e4d40c3a2e6426cd9b4084b49
org <ogamespec@gmail.com>
2024-09-03 11:20:00 +0300

    decoder.cpp

--- sim/sm83sim/sm83.cpp
+++ sim/sm83sim/sm83.cpp
@@ -4,6 +4,10 @@ namespace dmg
 {
 	void sm83_sim(sm83_state* st_old, sm83_state* st_now)
 	{
+		sm83_decoder1(st_now);
+		sm83_decoder2(st_now);
+		sm83_decoder3(st_now);
+
 		sm83_thingy_sim(st_now);
 	}
 }
--- sim/sm83sim/sm83.h
+++ sim/sm83sim/sm83.h
@@ -13,6 +13,7 @@ namespace dmg
 		int CLK;
 		int CLK1, CLK2, CLK3, CLK4, CLK5, CLK6, CLK7, CLK8, CLK9;
 
+		int a[26];
 		int d[107];
 		int w[41];
 		int x[69];
@@ -23,8 +24,14 @@ namespace dmg
 		int TTB2;
 		int TTB3;
 		int Thingy_to_bot;
+
+		int IR[8];
+		int nIR[6];
 	};
 
+	void sm83_decoder1(sm83_state* st);
+	void sm83_decoder2(sm83_state* st);
+	void sm83_decoder3(sm83_state* st);
 	void sm83_thingy_sim(sm83_state* st);
 
 	void sm83_trace(sm83_state* st);
